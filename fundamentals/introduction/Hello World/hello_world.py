603e30a4c0e21b3facc0f55ff17c0ac9752e684c
skarim <s.karim.jur@gmail.com>
2022-04-15 22:26:14 -0400

    initial commit

---
+++ fundamentals/introduction/Hello World/hello_world.py
@@ -0,0 +1,12 @@
+print("Hello World")
+name = "Noelle"
+print("Hello", name)
+print("Hello " + name )
+name = 42
+print("hello", 42 )
+print("Hello" + 42 )
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat" .format(fave_food1, fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}")
+
